ce6c76701b6398fb541b48960884625f54c1ce80
somy <fuureelf@naver.com>
2025-05-23 01:30:08 +0900

    dummy api 추가

--- app/tortoise_models/meeting.py
+++ app/tortoise_models/meeting.py
@@ -1,10 +1,18 @@
 from __future__ import annotations
+
+from datetime import date
+
 from tortoise import Model, fields
+
 from app.tortoise_models.base_model import BaseModel
 
 
 class MeetingModel(BaseModel, Model):
     url_code = fields.CharField(max_length=255, unique=True)
+    title = fields.CharField(max_length=255, default="")
+    location = fields.CharField(max_length=255, default="")
+    start_date = fields.DateField(null=True)
+    end_date = fields.DateField(null=True)
 
     class Meta:
         table = "meetings"
@@ -13,7 +21,10 @@ class MeetingModel(BaseModel, Model):
     async def create_meeting(cls, url_code: str) -> MeetingModel:
         return await cls.create(url_code=url_code)
 
-
     @classmethod
     async def get_by_url_code(cls, url_code: str) -> MeetingModel | None:
         return await cls.filter(url_code=url_code).get_or_none()
+
+    @classmethod
+    async def update_start_and_end(cls, url_code: str, start_date: date, end_date: date) -> None:
+        await cls.filter(url_code=url_code).update(start_date=start_date, end_date=end_date)

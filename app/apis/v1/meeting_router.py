de3d9c81445ac2771d34b9a4a19364a627459a40
somy <fuureelf@naver.com>
2025-05-22 23:53:38 +0900

    조회 API 를 구현

--- app/apis/v1/meeting_router.py
+++ app/apis/v1/meeting_router.py
@@ -1,8 +1,7 @@
 from fastapi import APIRouter
 
 from app.dtos.create_meeting_response import CreateMeetingResponse
-from app.services.meeting_service_edgedb import service_create_meeting_edgedb
-from app.services.meeting_service_mysql import service_create_meeting_mysql
+from app.services.meeting_service_mysql import service_create_meeting_mysql, service_get_meeting_mysql
 from fastapi import APIRouter, HTTPException
 from starlette.status import HTTP_404_NOT_FOUND
 
@@ -45,5 +44,11 @@ async def api_get_meeting_edgedb(meeting_url_code: str) -> GetMeetingResponse:
     description="meeting 을 조회합니다.",
 )
 async def api_get_meeting_mysql(meeting_url_code: str) -> GetMeetingResponse:
-    return GetMeetingResponse(url_code="abc")
+    meeting = await service_get_meeting_mysql(meeting_url_code)
+    if meeting is None:
+        raise HTTPException(
+            status_code=HTTP_404_NOT_FOUND, detail=f"meeting with url_code: {meeting_url_code} not found"
+        )
+    return GetMeetingResponse(url_code=meeting.url_code)
+
 
--- app/tortoise_models/meeting.py
+++ app/tortoise_models/meeting.py
@@ -1,7 +1,5 @@
 from __future__ import annotations
-
 from tortoise import Model, fields
-
 from app.tortoise_models.base_model import BaseModel
 
 
@@ -14,3 +12,8 @@ class MeetingModel(BaseModel, Model):
     @classmethod
     async def create_meeting(cls, url_code: str) -> MeetingModel:
         return await cls.create(url_code=url_code)
+
+
+    @classmethod
+    async def get_by_url_code(cls, url_code: str) -> MeetingModel | None:
+        return await cls.filter(url_code=url_code).get_or_none()

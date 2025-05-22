6bfde621d8bc82ab8742165cec1871a3a7074bc2
somy <fuureelf@naver.com>
2025-05-22 16:15:19 +0900

    aerich 로 마이그레이션

---
+++ app/tortoise_models/base_model.py
@@ -0,0 +1,11 @@
+from tortoise import fields
+
+
+class BaseModel:
+    id = fields.BigIntField(primary_key=True)
+    created_at = fields.DatetimeField(auto_now_add=True)
+
+#Mysql pk 를 정할 때 주의할 점
+#innodb의 특징 중 하나 -> clustering index
+#pk를 기준으로 값이 비슷한 row들 끼리 디스크에서도 실제로 모여있음
+# 랜덤 IO가 느리고 순차 IO가 빠름
